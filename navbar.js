6f5c426b96ad0820f54b03eb3971d41d22f2462d
Esmé Puzio <esmepuzio@Esmes-MacBook-Pro.local>
2023-11-08 12:47:52 +0000

    wip

--- navbar.js
+++ navbar.js
@@ -1,4 +1,4 @@
-//taken from https://templaterr.neocities.org/topbar/ !
+//referenced from https://templaterr.neocities.org/topbar/
 
 var html = '<div class="navlink"><a href="index.html">Home</a></div>';
 
@@ -7,4 +7,4 @@ for (i = 0; i<section1.length; i++){
 	html += '<div class="navlink"><a href="' + section1[i] + '.html">' + section1[i] + '</a></div>'
 }
 
-document.getElementById("templaterr-topbar").innerHTML = html;
+document.getElementById("navbar-desktop").innerHTML = html;
